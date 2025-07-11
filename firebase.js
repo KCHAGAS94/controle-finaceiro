983b128dda0f247108907b5c0f43523a0abc78c3
kchagas94 <kenedychagas@gmail.com>
2025-07-11 00:54:21 -0300

    add-firebase

--- firebase.js
+++ firebase.js
@@ -2,13 +2,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyB6WdD4hjf9LI9w3o5d2zLrtvL9JBRjNOI",
   authDomain: "controle-supermercado.firebaseapp.com",
   projectId: "controle-supermercado",
-  storageBucket: "controle-supermercado.appspot.com", // <-- Corrigido aqui
+  storageBucket: "controle-supermercado.appspot.com", 
   messagingSenderId: "437292484626",
   appId: "1:437292484626:web:5cdb07c2f03c8e050730cb"
-};
+}
 
 // Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
 
-// Inicializa o Firestore
-const db = firebase.firestore();
